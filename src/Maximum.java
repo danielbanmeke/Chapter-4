a3aee67c19c06b2625c56db96bd69df4fcfabad4
danielbanmeke <banmekedaniel@gmail.com>
2023-11-01 00:09:31 +0100

    Update all files

---
+++ src/Maximum.java
@@ -0,0 +1,15 @@
+public class Maximum {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 10;
+
+        if(x > y)
+            System.out.println("The maximum is " + x);
+
+        if(y > x)
+            System.out.println("The maximum is " + y);
+
+        if(y == x)
+            System.out.println("The numbers are equal");
+    }
+}
