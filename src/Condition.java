de56efa0c5cd3e723cc19a704db7bd01cf0934a5
danielbanmeke <banmekedaniel@gmail.com>
2023-11-01 10:31:20 +0100

    Update all files

---
+++ src/Condition.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Condition {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+        int n = input.nextInt();
+
+        if (n > 10)
+            System.out.println("The number is greater than 10");
+        else if (n == 10)
+            System.out.println("The number is equal to 10");
+        else
+            System.out.println("The number is less than 10");
+    }
+}
---
+++ src/OddAndEven.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class OddAndEven {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter an number: ");
+
+        if(input.nextInt() % 2 == 0) {
+            System.out.println("The number is even.");
+        }
+
+        else {
+            System.out.println("The number is odd");
+        }
+    }
+}
